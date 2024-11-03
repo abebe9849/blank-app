dfa9f28778b02ab26ad00a7542d277a0f6c0e9a8
Masatoshi Abe <55616956+abebe9849@users.noreply.github.com>
2024-11-03 19:43:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ image_files = glob.glob(f"{img_path}/*")  # 画像ファイルのリスト
 # 現在の画像インデックス
 if 'index' not in st.session_state:
     st.session_state.index = 0
+    st.session_state.all_imgs = len(image_files)
 
 # アノテーションデータのロードまたは初期化
 annotations_file = "./data/annotations.json"
@@ -23,58 +24,53 @@ else:
     annotations = {}
 
 image_files = [f for f in image_files if f not in annotations.keys()]
-print(image_files)
 
 
-if 'sidebar_closed' not in st.session_state:
-    st.session_state.sidebar_closed = False
 # テンプレート辞書
 templates = {
     "a": {
-        "0": "",
-        "1": "管内増殖が一部に見られます",
-        "2": "管内増殖が全節性に見られます",
-        "3": "所見a3のテンプレート文章",
-        "4": "線維性の半月体が見られます",
-        "5": "繊維細胞性の半月体が見られます",
-        "6": "細胞性の半月体が見られます",
-        "7": "小半月体が見られます",
-        "8": "管外に上皮細胞の増殖が見られます",
-        "9": "ボーマン嚢への癒着が見られます",
-        
+        "0": "管内性細胞増生が一部に見られます",
+        "1": "管内性細胞増生が全節性に見られます",
     },
     "b": {
-        "0": "",
-        "1": "所見b1のテンプレート文章",
-        "2": "メサンギウム細胞増多が見られます",
-        "5": "核崩壊が見られます",
-        "6": "硝子様の血栓が見られます",
-        "7": "係蹄腔の狭小化が見られます",
-        "8": "係蹄に壊死が見られます",
+        "0": "線維性半月体が見られます",
+        "1": "繊維細胞性半月体が見られます",
+        "2": "細胞性半月体が見られます",
+        "3": "小半月体が見られます",
+        "4": "係蹄の癒着が見られます",
+        "5": "尿腔内出血",## 浸出が見られます",
+        "6": "上皮細胞の増殖が見られます",
+        "7": "カプスラードロップが見られます",
     },
     "c": {
-        "0": "",
-        "1": "基底膜の二重化が分節性に見られます",
-        "2": "基底膜の二重化が全節性に見られます",
-        "3": "基底膜にスパイク/点刻像が疑われます",
-        "4": "糸球体は分葉状を呈しています",
-        "5": "半月体が全周性に見られます",
-        "6": "半月体が全周性に見られます",
-        "7": "基底膜の肥厚を認めます",
-        "7": "基底膜に沈着様の物質が疑われます",
-        
+        "0": "基底膜の肥厚が見られます",
+        "1": "基底膜にスパイクを認めます",
+        "2": "基底膜に断裂を認めます",
+        "3": "基底膜に沈着様の物質の存在が疑われます",
+        "4": "基底膜に二重化を認めます",
+        "5": "係蹄壊死を認めます"
+    },
+    "m": {#メサンギウム
+        "0": "メサンギウム細胞の軽度増殖を認めます",
+        "1": "メサンギウム細胞の中程度増殖を認めます",
+        "2": "メサンギウム細胞の重度増殖を認めます",
+        "3": "メサンギウム基質の増加が見られます",
+        "4": "メサンギウムの融解が見られます",
+        "5": "",
     },
     "d": {
-        "0": "",
-        "1": "全節性硬化が見られます",
+        "0": "全節性硬化が見られます",
+        "1": "分節性の硬化が見られます",
         "2": "結節性硬化が疑われます",
-        "3": "分節性の硬化が見られます",
-        "4": "結節性硬化が疑われます",
     },
     "e": {
-        "0": "光学顕微鏡では特記すべき所見はありません",
-        "1": "糸球体周囲の血管新生が見られます",
-        "2": "細動脈の硝子様変化が見られます",
+        "0": "係蹄腔の虚脱が見られます",
+        "1": "係蹄腔の拡張が見られます",
+    },
+    "f": {#"血管新生","硝子様変化","硝子様血栓"]
+        "0": "糸球体周囲の血管新生が見られます",
+        "1": "細動脈の硝子様変化が見られます",
+        "2": "硝子様血栓が見られます",
     },
 }
 
@@ -83,93 +79,166 @@ current_image_file = image_files[st.session_state.index]
 
 path_ = f"{img_path}"+current_image_file.split("/")[-1]
 
-#tmp = df[df["path"]==path_]
-
-#target,label,BMI,DBP,DM,age,alb,egfr,uprot,収縮期血圧,血尿
-
 # 全角を半角に変換する関数
 def zenkaku_to_hankaku(text):
     return text.translate(str.maketrans('０１２３４５６７８９', '0123456789'))
 
 # ページ設定で横幅を広げる
 st.set_page_config(layout="wide")
-
-
-#st.write(f"未アノテーションの画像数: {len(image_files)}")
-
-
-k = ["管内/管外増殖","メサンギウム領域","基底膜","硬化","周囲血管"]
-# サイドバーのテンプレートの表示
-st.sidebar.write("テンプレートの対応表")
-for i,key in enumerate(templates):
-    st.sidebar.write(f" {k[i]}:")
-    for num, text in templates[key].items():
-        st.sidebar.write(f"{num}: {text}")
-
-
-def change_value():
-    st.session_state["template_a"] = ""
-    st.session_state["template_b"] = ""
-    st.session_state["template_c"] = ""
-    st.session_state["template_d"] = "" 
-    st.session_state["template_e"] = "" 
-    # https://zenn.dev/alivelimb/books/python-web-frontend/viewer/about-streamlit
+form = st.form("checkboxes", clear_on_submit = True)
 
 # レイアウト設定
-col1, col2 = st.columns([1, 2])
+if st.session_state.sidebar_closed:
+    max_width = 400  # 800 * 0.9
+    max_height = 400  # 600 * 0.9
+else:
+    max_width = 500
+    max_height = 500
+# 画像の表示
+def resize_image(image_path, max_width=450, max_height=450):
+    image = Image.open(image_path)
+    image = image.resize((max_width, max_height))
+    return image
+resized_image = resize_image(current_image_file, max_width, max_height)
 #st.session_state['annotation_text'] = ""
 # テキストアノテーションの入力
-with col1:
-    st.write(f"未アノテーションの画像数: {len(image_files)}")
-    target__ = "DMN" #str(tmp["target"].values[0])
-    print(target__)
-    bp = 90 #tmp["収縮期血圧"].values[0]
-    egfr = 60#int(tmp["egfr"].values[0])
-    ubp = 3.5#tmp["uprot"].values[0]
-    hep = 0.5 #tmp["血尿"].values[0]
-    st.write(f"{target__}")
+with form:
+    col1, col2 = st.columns([4, 4])
+    with col1:
+        
+
+        
+        checkbox_labels = ["管内細胞増殖:一部","管内細胞増殖:全節"]  # 5個のチェックボックスのラベル
+        cols = st.columns(len(checkbox_labels))  # チェックボックスの数だけ列を作成
+        checkbox_states_a = []
+
+        for i, label in enumerate(checkbox_labels):
+            with cols[i]:  # 各列にチェックボックスを配置
+                checkbox_state = st.checkbox(label, key=label,help=templates["a"][str(i)])
+                checkbox_states_a.append(checkbox_state)
+
+        st.write("======管外病変======")
+        checkbox_labels = ["線維性半月体","繊維細胞性半月体","細胞性半月体","小半月体","癒着","尿腔内出血 浸出","上皮細胞の増殖","カプスラードロップ"]  # 5個のチェックボックスのラベル
+        #cols = st.columns(len(checkbox_labels))  # チェックボックスの数だけ列を作成
+        checkbox_states_b = []
+        rows = [checkbox_labels[:4], checkbox_labels[4:]]
+        for row in rows:
+            cols = st.columns(4)
+
+            for i, label in enumerate(row):
+                with cols[i]:  # 各列にチェックボックスを配置
+                    checkbox_state = st.checkbox(label, key=label,help=templates["b"][str(i)])
+                    checkbox_states_b.append(checkbox_state)
+
+
+        st.write("======メサンギウム======")
+        checkbox_labels = ["軽度 細胞増殖","中程度 細胞増殖","重度 細胞増殖","基質の増加","融解"]  # 5個のチェックボックスのラベル
+        cols = st.columns(len(checkbox_labels))  # チェックボックスの数だけ列を作成
+        checkbox_states_m = []
+
+        for i, label in enumerate(checkbox_labels):
+            with cols[i]:  # 各列にチェックボックスを配置
+                checkbox_state = st.checkbox(label, key=label,help=templates["m"][str(i)])
+                checkbox_states_m.append(checkbox_state)
+
+        st.write("======基底膜======")
+        checkbox_labels = ["肥厚","スパイク","断裂","沈着","二重化","係蹄壊死"]  # 5個のチェックボックスのラベル
+        cols = st.columns(len(checkbox_labels))  # チェックボックスの数だけ列を作成
+        checkbox_states_c = []
+
+        for i, label in enumerate(checkbox_labels):
+            with cols[i]:  # 各列にチェックボックスを配置
+                checkbox_state = st.checkbox(label, key=label,help=templates["c"][str(i)])
+                checkbox_states_c.append(checkbox_state)
+        st.write("======その他======")
+        checkbox_labels = ["全節性硬化","結節性硬化","分節性硬化"]  # 5個のチェックボックスのラベル
+        cols = st.columns(len(checkbox_labels))  # チェックボックスの数だけ列を作成
+        checkbox_states_d = []
+
+        for i, label in enumerate(checkbox_labels):
+            with cols[i]:  # 各列にチェックボックスを配置
+                checkbox_state = st.checkbox(label, key=label,help=templates["d"][str(i)])
+                checkbox_states_d.append(checkbox_state)
+        checkbox_labels_e = ["係蹄腔虚脱","係蹄腔拡張"]  # 5個のチェックボックスのラベル
+        cols = st.columns(len(checkbox_labels_e))  # チェックボックスの数だけ列を作成
+        checkbox_states_e = []
+
+        for i, label in enumerate(checkbox_labels_e):
+            with cols[i]:  # 各列にチェックボックスを配置
+                checkbox_state = st.checkbox(label, key=label,help=templates["e"][str(i)])
+                checkbox_states_e.append(checkbox_state)
+
+        checkbox_labels_f = ["血管新生","硝子様変化","硝子様血栓"]  # 5個のチェックボックスのラベル
+        cols = st.columns(len(checkbox_labels_f))  # チェックボックスの数だけ列を作成
+        checkbox_states_f = []
+
+        for i, label in enumerate(checkbox_labels_f):
+            with cols[i]:  # 各列にチェックボックスを配置
+                checkbox_state = st.checkbox(label, key=label,help=templates["f"][str(i)])
+                checkbox_states_f.append(checkbox_state)
 
 
 
-    st.write(f"収縮期血圧:{bp},egfr:{egfr},尿蛋白:{ubp},血尿:{hep}")
+    with col2:
 
-    # テンプレート番号の入力
-    template_a = zenkaku_to_hankaku(st.text_input("管内/管外増殖", key="template_a"))
-    template_b = zenkaku_to_hankaku(st.text_input("メサンギウム領域", key="template_b"))
-    template_c = zenkaku_to_hankaku(st.text_input("基底膜", key="template_c"))
-    template_d = zenkaku_to_hankaku(st.text_input("硬化", key="template_d"))
-    template_e = zenkaku_to_hankaku(st.text_input("周囲血管", key="template_e"))
+        #st.markdown('<div class="image-container">', unsafe_allow_html=True)
+        
+        target__ = "DMN" #str(tmp["target"].values[0])
+        print(target__)
+        bp = 90 #tmp["収縮期血圧"].values[0]
+        egfr = 60#int(tmp["egfr"].values[0])
+        ubp = 3.5#tmp["uprot"].values[0]
+        hep = 0.5 #tmp["血尿"].values[0]
+        caption_text= f"収縮期血圧:{bp},eGFR:{egfr},尿蛋白:{ubp},血尿:{hep}"
+        st.image(resized_image, use_column_width=True)
+        st.markdown(f"<div style='text-align: center; font-size: 24px; font-weight: bold;'>{caption_text}</div>", unsafe_allow_html=True)
+submit = form.form_submit_button("テンプレートを適用")
 
-    # 自由記述欄の初期値設定
-    if 'annotation_text' not in st.session_state:
-        st.session_state['annotation_text'] = annotations.get(current_image_file, "")
-
-    # テンプレート文章の取得
-    if st.button("テンプレートを適用"):
-        for t in template_a:
-            if t in templates["a"]:
-                st.session_state['annotation_text'] += templates["a"][t] + "\n"
-        for t in template_b:
-            if t in templates["b"]:
-                st.session_state['annotation_text'] += templates["b"][t] + "\n"
-        for t in template_c:
-            if t in templates["c"]:
-                st.session_state['annotation_text'] += templates["c"][t] + "\n"
-        for t in template_d:
-            if t in templates["d"]:
-                st.session_state['annotation_text'] += templates["d"][t] + "\n"
-        for t in template_e:
-            if t in templates["e"]:
-                st.session_state['annotation_text'] += templates["e"][t] + "\n"
-        print(st.session_state['annotation_text'])
+# 自由記述欄の初期値設定
+if 'annotation_text' not in st.session_state:
+    st.session_state['annotation_text'] = annotations.get(current_image_file, "")
+#if st.button("テンプレートを適用"):
+if submit:
+    print(checkbox_states_a)
+    for idx,c in enumerate(checkbox_states_a):
+        if c:
+            st.session_state["annotation_text"] += templates["a"][str(idx)] + "\n"
+    for idx,c in enumerate(checkbox_states_b):
+        if c:
+            st.session_state["annotation_text"] += templates["b"][str(idx)] + "\n"
+    for idx,c in enumerate(checkbox_states_c):
+        if c:
+            st.session_state["annotation_text"] += templates["c"][str(idx)] + "\n"
+    for idx,c in enumerate(checkbox_states_m):
+        if c:
+            st.session_state["annotation_text"] += templates["m"][str(idx)] + "\n"
+    for idx,c in enumerate(checkbox_states_d):
+        if c:
+            st.session_state["annotation_text"] += templates["d"][str(idx)] + "\n"
+    for idx,c in enumerate(checkbox_states_e):
+        if c:
+            st.session_state["annotation_text"] += templates["e"][str(idx)] + "\n"
+    for idx,c in enumerate(checkbox_states_f):
+        if c:
+            st.session_state["annotation_text"] += templates["f"][str(idx)] + "\n"
     
 
-    annotation_text = st.text_area("テキスト", value=st.session_state['annotation_text'], height=200)
+    print(st.session_state['annotation_text'])
+
+
+col_1, col_2 = st.columns([1, 1])
+
+with col_1:
+    annotation_text = st.text_area("テキスト", value=st.session_state['annotation_text'], height=400)
     # 完了ボタンとクリアボタンを横に並べる
+    st.write(f"未アノテーションの画像数: {len(image_files)} / {st.session_state.all_imgs}")
+
     col_button1, col_button2 = st.columns([1, 1])
     with col_button1:
+
         
-        if st.button("完了", on_click=change_value):            
+        
+        if st.button("完了"):           
             annotations[current_image_file] = annotation_text
             with open(annotations_file, 'w', encoding='utf-8') as f:
                 json.dump(annotations, f, ensure_ascii=False, indent=4)
@@ -180,6 +249,8 @@ with col1:
             #if st.session_state.index >= len(image_files):
             #    st.session_state.index = 0
             if len(image_files) == 1:
+                print("last")
+                #download annotation json
                 st.session_state.index = 0
                 # アノテーションJSONファイルをダウンロード
                 with open(annotations_file, 'r', encoding='utf-8') as f:
@@ -193,14 +264,10 @@ with col1:
 
                 
                 st.success("すべての画像にアノテーションが完了しました")
-                
-                
             st.session_state['annotation_text'] = ""
             
             print(st.session_state)
             
-
-
             if len(image_files) != 1:
 
                 st.rerun()
@@ -209,27 +276,21 @@ with col1:
             
 
             
-
     with col_button2:
+        
         if st.button("クリア"):
             st.session_state['annotation_text'] = ""
             st.rerun()
-if st.session_state.sidebar_closed:
-    max_width = 400  # 800 * 0.9
-    max_height = 400  # 600 * 0.9
-else:
-    max_width = 500
-    max_height = 500
-# 画像の表示
-def resize_image(image_path, max_width=450, max_height=450):
-    image = Image.open(image_path)
-    image = image.resize((max_width, max_height))
-    return image
 
-with col2:
-    #st.markdown('<div class="image-container">', unsafe_allow_html=True)
-    resized_image = resize_image(current_image_file, max_width, max_height)
-    st.image(resized_image, caption=current_image_file, use_column_width=True)
+with col_2:
+    target__ = "DMN" #str(tmp["target"].values[0])
+    bp = 90 #tmp["収縮期血圧"].values[0]
+    egfr = 60#int(tmp["egfr"].values[0])
+    ubp = 3.5#tmp["uprot"].values[0]
+    hep = 0.5 #tmp["血尿"].values[0]
+    caption_text= f"収縮期血圧:{bp},eGFR:{egfr},尿蛋白:{ubp},血尿:{hep}"
+    st.image(resized_image, use_column_width=True)
+    st.markdown(f"<div style='text-align: center; font-size: 24px; font-weight: bold;'>{caption_text}</div>", unsafe_allow_html=True)
 
-    #st.markdown('</div>', unsafe_allow_html=True)
+
 
